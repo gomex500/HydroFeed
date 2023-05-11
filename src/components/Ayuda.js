d4bde2dd879409c0fb9142e505e318579ea16ad7
gomex500 <gomezfreddy886@gmail.com>
2023-05-10 20:12:46 -0600

    creacion de apartado de ayuda o por lo menos la parte visual

--- src/components/Ayuda.js
+++ src/components/Ayuda.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
+import '../css/ayuda.css'
 
 var Ayuda = () =>{
+    const [mensaje, setMensaje] = useState([]);
+    const [newMensaje, setNewMensaje] = useState('');
+
+    const optenerMensaje = (event) => {
+      setNewMensaje(event.target.value);
+    };
+
+    const enviarMensaje = (event) => {
+      event.preventDefault();
+      if (newMensaje.trim() !== '') {
+        const message = {
+          content: newMensaje,
+          author: 'Usuario',
+          timestamp: new Date().toLocaleTimeString(),
+        };
+        setMensaje([...mensaje, message]);
+        setNewMensaje('');
+      }
+    };
+
+
+
     return(
-        <h1>Ayuda</h1>
+        <div className="container-fluid">
+            <div className="mensajes">
+                {mensaje.map((message, index) => (
+                <div key={index}>
+                    <strong>{message.author}: </strong>
+                    <span>{message.content}</span>
+                </div>
+                ))}
+            </div>
+            <div className="mensaje">
+                <form onSubmit={enviarMensaje}>
+                    <input type='text' className="form-control inpt" value={newMensaje} onChange={optenerMensaje} placeholder="ingrese su pregunda"/>
+                    <button className="btn btn1"><i class="bi bi-send-fill"></i></button>
+                </form>
+            </div>
+        </div>
     )
 }
 
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -2,6 +2,7 @@ import React from "react";
 import '../css/inicio.css'
 import logo from "../img/logoHydroFeed.png"
 import img2 from '../img/img2.png'
+import img1 from '../img/img1.png'
 
 var Inicio = () =>{
     return(
@@ -17,17 +18,29 @@ var Inicio = () =>{
                     </div>
                 </div>
             </div>
-            <div className="container-fluid row">
-            <div className="col-12 col-lg-8">
-                    <p className="p">En Nicaragua, la combinación de sequía prolongada y altas temperaturas, con registros que alcanzan los 36°C o 39°C, representa una problemática significativa para los ganaderos locales. Durante esta época del año, la falta de lluvia resulta en una disminución drástica de los pastizales y forraje natural, lo que conduce a una escasez de alimento para el ganado.</p>
+            <center>
+                <div className="container-fluid row">
+                    <h3 className="titulo">Problematica</h3>
+                    <div className="col-12 col-lg-8">
+                        <p className="p">En Nicaragua, la combinación de sequía prolongada y altas temperaturas, con registros que alcanzan los 36°C o 39°C, representa una problemática significativa para los ganaderos locales. Durante esta época del año, la falta de lluvia resulta en una disminución drástica de los pastizales y forraje natural, lo que conduce a una escasez de alimento para el ganado.</p>
+                    </div>
+                    <div className="col-12 col-lg-4">
+                        <img src={img2} className='img2' alt='img2'/>
+                    </div>
+                    <div className="col-12">
+                        <p className="p">En este contexto, es esencial encontrar soluciones adaptadas a las condiciones de Nicaragua que permitan superar esta problemática. El cultivo de forraje hidropónico se presenta como una alternativa viable, ya que permite a los ganaderos producir alimento nutritivo en un ambiente controlado y sin depender de la disponibilidad de agua de lluvia. El forraje hidropónico puede ser cultivado en sistemas cerrados, utilizando tecnologías que optimizan el uso del agua y regulan las condiciones de temperatura, permitiendo así el suministro constante de alimento para el ganado, incluso durante la sequía y las altas temperaturas.</p>
+                    </div>
                 </div>
-                <div className="col-12 col-lg-4">
-                    <img src={img2} className='img2' alt='img2'/>
+                <div className="container-fluid row">
+                    <h3 className="titulo">Forraje Hidropónico</h3>
+                    <div className="col-12 col-lg-4">
+                        <img src={img1} className='img3' alt='img3'/>
+                    </div>
+                    <div className="col-12 col-lg-8">
+                        <p className="p">El forraje hidropónico (F.H) es un sistema de cultivos utilizado para producir alimentos a partir de una solución acuosa y nutritiva. Es decir, no se utiliza un sustrato de tierra para la siembra de granos y leguminosas. Usualmente es un modelo de producción para alimentar bovinos, ovinos y caprinos pues son cultivos de producción rápida y que se puede masificar.</p>
+                    </div>
                 </div>
-                <div className="col-12">
-                    <p className="p2">En este contexto, es esencial encontrar soluciones adaptadas a las condiciones de Nicaragua que permitan superar esta problemática. El cultivo de forraje hidropónico se presenta como una alternativa viable, ya que permite a los ganaderos producir alimento nutritivo en un ambiente controlado y sin depender de la disponibilidad de agua de lluvia. El forraje hidropónico puede ser cultivado en sistemas cerrados, utilizando tecnologías que optimizan el uso del agua y regulan las condiciones de temperatura, permitiendo así el suministro constante de alimento para el ganado, incluso durante la sequía y las altas temperaturas.</p>
-                </div>
-            </div>
+            </center>
         </div>
     )
 }
